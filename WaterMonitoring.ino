eb975b0d397913ba25ef0834389dd6e9d0d883dc
fandhikazhar <99522867+fandhikazhr@users.noreply.github.com>
2022-02-12 21:08:23 +0700

    Update WaterMonitoring.ino

--- WaterMonitoring.ino
+++ WaterMonitoring.ino
@@ -1,36 +1,66 @@
-#define buzzer D0
-#define triggerPin D1
-#define echoPin D2
+#define trigPin 2
+#define echoPin 3
+
+int LED1 = 8;
+int LED2 = 9;
+int LED3 = 10;
+int BUZZER = 11;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(triggerPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(buzzer, OUTPUT);
   Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  // put your setup code here, to run once:
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  long duration, jarak;
-  digitalWrite(triggerPin, LOW);
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  digitalWrite(triggerPin, HIGH);
+  digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
+  digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
-  jarak = (duration/2) / 29.1;
-  Serial.print("Jarak Sensor : ");
-  Serial.println(jarak);
-  if(jarak < 10)
+  distance = (duration/2) / 29.1;
+  Serial.println(distance);
+
+  if(distance <= 25)
   {
-    digitalWrite(buzzer, HIGH);
-    delay(100);
-    digitalWrite(buzzer, LOW);
+    digitalWrite(LED1, HIGH);
   }
   else
   {
-    digitalWrite(buzzer, LOW);
-    delay(1000);
+    digitalWrite(LED1, LOW);
   }
+  if(distance <= 20)
+  {
+    digitalWrite(LED2, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED2, LOW);
+  }
+  if(distance <= 15)
+  {
+    digitalWrite(LED3, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED3, LOW);
+  }
+  if(distance <= 10)
+  {
+    digitalWrite(BUZZER, HIGH);
+  }
+  else
+  {
+    digitalWrite(BUZZER, LOW);
+  }
+  // put your main code here, to run repeatedly:
+
 }
